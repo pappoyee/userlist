ee21f5c9815664fb45b9d3d0bcdc9fd535b51e39
Siljy VK <siljy.k@visualyze.ai>
2020-11-19 03:31:56 +0530

    removed debugger from app

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -6,21 +6,19 @@ export default ({ user }) => {
     const dispatch = useDispatch();
 
     const onChangeName = useCallback((v) => {
-        debugger;
         const newName = v.target.value;
         dispatch(updateUser({ ...user, name: newName }))
     })
 
     const onChangeEmail = useCallback((v) => {
-        debugger;
         const newEmail = v.target.value;
         dispatch(updateUser({ ...user, email: newEmail }))
     })
     return <div className="row container">
-        <span className="col s6 input-field">
+        <span className="col s6">
             <input onChange={onChangeName} value={user?.name} />
         </span>
-        <span className="col s6 input-field">
+        <span className="col s6">
             <input onChange={onChangeEmail} value={user?.email} />
         </span>
     </div>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,7 +5,6 @@ const intialState = [];
 export const reducer = (state = intialState, action) => {
   switch (action.type) {
     case ADD_USER:
-      debugger;
       const nextId = state.length + 1;
       const last = state[state.length - 1];
 
@@ -20,12 +19,10 @@ export const reducer = (state = intialState, action) => {
       };
       return [...state, newUser];
     case UPDATE_USER:
-      debugger;
       const index = state.findIndex((u) => u.id === action.payload.id);
       state[index] = action.payload;
       return [...state];
     case LOAD_INITIAL:
-      debugger;
       return [...action.payload];
     default:
       return state;
